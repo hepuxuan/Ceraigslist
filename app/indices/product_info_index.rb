50f961050e711b210a1c0bc903ded0ca0d593b64
Puxuan He <puxuan.he@cerner.com>
2013-10-30 21:52:24 -0500

    text

--- app/indices/product_info_index.rb
+++ app/indices/product_info_index.rb
@@ -1,5 +1,5 @@
 ThinkingSphinx::Index.define :product_info, :with => :active_record do
-  indexes [title, body, tag_list.join(" ")], as: :title_body
+  indexes [title, body], as: :title_body
   index price, as: :price
   has price, post_date
 end
