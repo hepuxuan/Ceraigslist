cd2ade91669dc6109cb3e2ac908d58a54982aa9f
Puxuan He <puxuan.he@cerner.com>
2013-11-01 16:35:42 -0500

    fix tag

--- app/controllers/product_infos_controller.rb
+++ app/controllers/product_infos_controller.rb
@@ -13,12 +13,12 @@ class ProductInfosController < ApplicationController
     else
       order = 'post_date DESC'
     end
-    if params[:tag].present? || (params[:search] && (ActsAsTaggableOn::Tag.all.include?(params[:search])))
+    if params[:tag].present?
       tag = params[:tag]? params[:tag] : params[:search]
       @product_infos = ProductInfo.tagged_with(tag).where('price <= ? AND price >= ?', price_max, price_min).order(order).to_a.paginate(:page => params[:page], per_page: per_page)
     elsif params[:search].present?
       @product_infos = ProductInfo.search conditions: {title_body: params[:search]}, with: {price: price_min..price_max},
-        order: order,page: params[:page], per_page: per_page  
+        order: order, page: params[:page], per_page: per_page  
     else
       @product_infos = ProductInfo.where('price <= ? AND price >= ?', price_max, price_min).order(order).paginate(:page => params[:page], per_page: per_page)
     end
