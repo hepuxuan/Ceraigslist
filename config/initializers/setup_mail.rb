0b8ccf0963b357392d6634823c02a9bfc8d86070
Puxuan He <puxuan.he@cerner.com>
2013-11-05 21:18:08 -0600

    add forgot password

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,6 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => "gmail.com",
   :user_name            => "puxuanhe",
   :password             => "he7962787",
   :authentication       => "plain",
