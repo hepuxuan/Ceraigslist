541c867f232f50622b5ec698ff0a0107bb923913
Puxuan He <puxuan.he@cerner.com>
2013-11-05 23:17:56 -0600

    fix edit user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,8 @@ class User < ActiveRecord::Base
 
   before_save{|user| user.email = user.email.downcase} 
   #validates_presence_of :name, :address, :state, :city
-  validates_presence_of :password, :on => :create
-  validates_presence_of :password_confirmation, :on => :create
-  
-  validates :password, :confirmation => true,
-                       :length => {:within => 6..40}
+  validates :password, presence: true, length: {minimum: 6, maximum: 120}, on: :create
+  validates :password, length: {minimum: 6, maximum: 120}, on: :update, allow_blank: true
 
   VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validates:email, presence: true, format:{with: VALID_EMAIL_REGEX}, uniqueness:{case_sensitive:false}
