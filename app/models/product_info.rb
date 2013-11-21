77dc8fa2a06cfbe51c30a5cc5802d449878ff729
Puxuan He <puxuan.he@cerner.com>
2013-11-21 00:52:45 -0600

    fix date

--- app/models/product_info.rb
+++ app/models/product_info.rb
@@ -6,14 +6,14 @@ class ProductInfo < ActiveRecord::Base
   attr_accessible :assets_attributes, :post_date, :title, :body, :price, :address, :user_id, :state, :city, :tag_list, :uri, :source, :product_id, :latitude, :longitude
   acts_as_taggable
   accepts_nested_attributes_for :assets
-
+  RATE = 0.0174532925
   LOCAL = 0
   CRAGLIST = 1
   before_save do |product_info|
   	if product_info.address.present?
       geo_loc = Geokit::Geocoders::GeonamesGeocoder.geocode product_info.address
-      product_info.latitude = geo_loc.lat
-      product_info.longitude = geo_loc.lng
+      product_info.latitude = geo_loc.lat * RATE
+      product_info.longitude = geo_loc.lng * RATE
     end
   end
 end
