fd1a2e7bf498ac28ab5306b9e5186a25aabfb438
Puxuan He <puxuan.he@cerner.com>
2014-01-15 00:48:49 -0600

    add processed

--- app/indices/product_info_index.rb
+++ app/indices/product_info_index.rb
@@ -1,7 +1,7 @@
 ThinkingSphinx::Index.define :product_info, with: :active_record do
   indexes [title, body, tag_list.join], as: :title_body
   index price, as: :price
-  indexes processed, as: :processed
+  has processed
   has price, post_date
   has latitude, longitude
 end
